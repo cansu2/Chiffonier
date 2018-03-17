582f6275d721ef5c95d8ae3f30cff9c726f0c2b9
joe arnold <joearnold@joes-MacBook-Air.local>
2018-03-16 19:19:51 -0600

    joe

---
+++ models/user.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      // Giving the Author model a name of type STRING
+      name: DataTypes.STRING
+    });
+  
+    User.associate = function(models) {
+      // Associating Author with casual clothing
+      // When a User is deleted, also delete any associated items
+      User.hasMany(models.Casual, {
+        onDelete: "cascade"
+      });
+    };
+
+    User.associate = function(models) {
+        // Associating Author with formal clothing
+        // When a User is deleted, also delete any associated items
+        User.hasMany(models.Formal, {
+          onDelete: "cascade"
+        });
+      };
+  
+    return Author;
+  };
